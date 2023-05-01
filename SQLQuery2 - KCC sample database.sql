d1bd32fdf97fe0fd1e0fac8dffb59941dc6353f9
ksrm <73263862+ksrm@users.noreply.github.com>
2023-05-01 10:48:41 -0500

    Update SQLQuery2 - KCC sample database.sql

--- SQLQuery2 - KCC sample database.sql
+++ SQLQuery2 - KCC sample database.sql
@@ -5,4 +5,10 @@ CustomerName,
 Phone,
 c.CustomerID
 From dbo.Orders o left outer Join dbo.Customers c on o.CustomerID = c.CustomerID
-Order by OrderTotal desc
+Order by OrderTotal desc;
+
+SELECT Customers.CustomerName, SUM(Orders.OrderTotal) AS [Total Sales]
+FROM     Orders RIGHT OUTER JOIN
+                  Customers ON Orders.CustomerID = Customers.CustomerID
+WHERE  (Orders.OrderDate >= DATEADD(year, - 2, GETDATE()))
+GROUP BY Customers.CustomerName;
